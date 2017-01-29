278e78c7a12c1f6e4aea41110948e9d0edc02164
Videlin Donchev <ya.5164646777752015@yandex.ru>
2017-01-29 16:30:22 +0200

    Replace round with number_format to preserve the two digits after decimal point

--- 01.Syntax/02.sumTwoNumbers.php
+++ 01.Syntax/02.sumTwoNumbers.php
@@ -2,5 +2,5 @@
 $firstNumber = 1234.5678;
 $secondNUmber = 333;
 
-$sum = round($firstNumber + $secondNUmber, 2);
+$sum = number_format($firstNumber + $secondNUmber, 2);
 echo '$firstNumber + $secondNumber = ' . $firstNumber . ' + ' . $secondNUmber . ' = ' . $sum;
